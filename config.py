fad660e32195b121a5df1d4f6d547fce029ba663
nyx69 <loool1912@web.de>
2022-10-28 14:04:13 +0200

    further stuff

--- config.py
+++ config.py
@@ -1,34 +1,79 @@
+import dataclasses
+import os
+import re
 
+from dotenv import load_dotenv
+
+load_dotenv()
+api_id = os.getenv("TG_ID")
+api_hash = os.getenv("TG_HASH")
 
 DATABASE_URL = "postgresql://postgres:area@localhost:5432/news"
 POST_CHANNEL = -1001839268196
 
+FLAG_EMOJI = re.compile(r"🏴|🏳️|([🇦-🇿]{2})")
+PLACEHOLDER = "║"
+
+@dataclasses.dataclass
+class Source:
+    channel_name: str
+    footer: str = r"\s*"
+    bias: str = ""
+
+
 CHANNELS = {
-    -1001839268196: "Test",  # Test
-    1317941240: "MN-Bot",  # MN-bot
-    -1001391125365: "Test2",  # Test2
-    -1001526741474: "MNChat",  # MNChat
+    # -1001839268196:Source("Test"),  # Test
+    # 1317941240:Source("MN-Bot",  # MN-bot
+    -1001391125365: Source("Test2"),  # Test2
+  #   -1001526741474:Source("MNChat"),  # MNChat
+    -1001486678205: Source("Militär-Memes", "🔰 Subscribe to @MilitaerMemes for more"),  # todo: also handle DE footer
+    -1001240262412: Source("Militär-News🔰", "🔰 Abonniere @MilitaerNews\n🔰 Diskutiere im @MNChat"),
 
-    -1001452778151: "Krugozor🇺🇦",  # Кругозор
-    -1001619608359: "TiskSnip🇺🇦",
-    -1001594428146: "WerochnaRada🇺🇦",
-    -1001469021333: "DeepState🇺🇦",
-    -1001759230769: "Bayraktar🇺🇦",
+    # Ukrainian
+    -1001452778151: Source("Krugozor", "<a href='https//t.me/krugozor_news'>Кругозор</a>", "🇺🇦"),
+    -1001619608359: Source("TiskSnip", bias="🇺🇦"),
+    -1001594428146: Source("WerochnaRada", bias="🇺🇦"),
+    -1001469021333: Source("DeepState", bias="🇺🇦"),
+    -1001759230769: Source("Bayraktar", "<a href='https://t.me/+WPl-WyPpW3QyMjgy'>Свежие крипто-новости тут</a>", "🇺🇦"),
+    -1001201459971: Source("Rysnya", bias="🇺🇦"),
+    -1001218743574: Source("Goncharenko", bias="🇺🇦"),
+    -1001413275904: Source("NEXTA", bias="🇺🇦"),
+    -1001011817559: Source("Militarnij", "@milinua", "🇺🇦"),
+    -1001414210176: Source("FirstDivision", bias="🇺🇦"),
 
-    -1001076431027: "ItsDonezk🇷🇺",
-    -1001260622817: "Readovka🇷🇺",  # Readovka
-    -1001149896996: "Interfax🇷🇺",
-    -1001543896043: "RussiaMedwed🇷🇺",
-    -1001135021433: "WarGonzo🇷🇺",
-    -1001708761316: "PrjamoyEfir🇷🇺",
-    -1001382288937: "BalkanSniper🇷🇺",
-    -1001099737840: "Rusvesna🇷🇺",
+    # Russian
+    -1001141171940: Source("Kadyrow", bias="🇷🇺"),
+    -1001076431027: Source("ItsDonezk",
+                           "<a href='https://t.me/joinchat/QCkIs9PWI4jWPgNy'>Подписаться</a>  |  <a href='https://t.me/EtoDonetsk_bot'>Предложить новость</a>",
+                           "🇷🇺"),
+    -1001260622817: Source("Readovka", bias="🇷🇺"),
+    -1001149896996: Source("Interfax", "@interfaxonline", "🇷🇺"),
+    -1001543896043: Source("RussiaMedwed",
+                           "Вся ЖЕСТЬ СВО у нас в резерве 👉 <a href='https://t.me/+bSe6VmuHMlthYWEy'>Смотреть</a>.",
+                           "🇷🇺"),
+    -1001135021433: Source("WarGonzo",
+                           "@wargonzo\n\n*наш проект существует на средства подписчиков, карта для помощи\n4279 3806 9842 9521",
+                           "🇷🇺"),
+    -1001708761316: Source("PrjamoyEfir", r"<a href='https://t.me/+jQvaOFH1zhAzMzIy'>Прямой эфир - подписаться</a>",
+                           "🇷🇺"),
+    -1001382288937: Source("BalkanSniper", bias="🇷🇺"),
+    -1001099737840: Source("Rusvesna",
+                           "<a href='<strong>t.me/rusvesnasu</strong>'><strong>t.me/rusvesnasu</strong></a>", "🇷🇺"),
+    -1001326223284: Source("Rybar", "@rybar\n\n<em>*Поддержать нас: </em><code>4377 7278 0407 7977</code>", "🇷🇺"),
+    -1001730870551: Source("RSOTM", bias="🇷🇺"),
 
-    -1001577023152: "Syrenka🇵🇱",
+    # Polish
+    -1001577023152: Source("Syrenka", "@pl_syrenka — Польша не заграница", "🇵🇱"),
 
-    -1001261746950: "AAF🇦🇿",
+    # Azeri
+    -1001261746950: Source("AAF",
+                           "<a href='http://t.me/military_az'><strong>AАF</strong></a><strong> | </strong><a href='https://t.me/joinchat/TCl5uh3G0Mhx42Kd'><strong>Чат24/7</strong></a><strong> |</strong><a href='https://t.me/AslanAslanoff'><strong>Сотрудничество</strong></a>",
+                           "🇦🇿"),
 
-    -1001658917464: "KhersonNonFake",
-    -1001731636769: "BatalionMonako",
-    -1001602838096: "KronikaBpla",
+    # Unclear
+    -1001658917464: Source("KhersonNonFake", "</strong><strong>@kherson_non_fake</strong>", "🇺🇦"),
+    -1001731636769: Source("Batalion «Monako»", "<a href='https://t.me/+un4Mel3_naU1ZjZi'>Батальон «Монако» 💎</a>"),
+    -1001602838096: Source("KronikaBpla", ),
+    -1001689244469: Source("UMF"),
+
 }
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import re
+
 from deep_translator import GoogleTranslator
 from telethon import TelegramClient, events
-from telethon.events import NewMessage
+from telethon.events import NewMessage, MessageEdited
 from telethon.tl.types import UpdateNewChannelMessage, UpdateEditChannelMessage
 
 import config
@@ -10,6 +12,39 @@ client = TelegramClient("remove_inactive", config.api_id, config.api_hash)
 client.parse_mode = 'html'
 
 
+def translate(event):
+    if event.message.fwd_from is not None and event.message.fwd_from.from_id in list(config.CHANNELS.keys()):
+        return
+
+    footer = config.CHANNELS[event.chat_id].footer.replace("/", r"\/").replace(".", r"\.").replace("+", r"\+").replace(
+        "'", "\"").replace("|", r"\|")
+    print(footer)
+    if footer != "":
+        matches = re.findall(footer, event.text)
+        print(matches, event.text)
+        if len(matches) == 0:
+            return  # because some have their ads without footer
+
+    sub_text = re.sub(r"\s*((#\w+)*\s*)*\s*" + footer + r"\s*$", '', event.text)
+    emojis = re.findall(config.FLAG_EMOJI, sub_text)
+    text_to_translate = re.sub(config.FLAG_EMOJI, config.PLACEHOLDER, sub_text)
+    translated_text = GoogleTranslator(target="de").translate(text=text_to_translate)
+    for emoji in emojis:
+        translated_text = re.sub(config.PLACEHOLDER, emoji, translated_text, 1)
+
+    print("translated_text :::::", translated_text)
+    return f"{translated_text}\n\n<i>Quelle: <a href='tg://privatepost?channel={str(event.chat_id)[4:]}&post={event.message.id}'>{config.CHANNELS[event.chat_id].channel_name}{config.CHANNELS[event.chat_id].bias}</a></i>\n\n👉🏼 Folge @NYX_News für mehr!"
+
+
+@client.on(events.Album(chats=list(config.CHANNELS.keys())))
+async def handle_album(event):  # craft a new message and send
+    print("album ------------------- ", event.stringify())
+    text = translate(event)
+
+    await client.send_message(config.POST_CHANNEL, file=event.messages, message=text, )
+    # ## or forward it directly # await event.forward_to(chat)
+
+
 @client.on(events.NewMessage(chats=list(config.CHANNELS.keys()), incoming=True))
 @client.on(events.NewMessage(chats=-1001391125365, outgoing=True))
 async def post_text(event: NewMessage.Event):
@@ -17,19 +52,16 @@ async def post_text(event: NewMessage.Event):
 
     print(event.stringify())
 
-    translated_text = GoogleTranslator(target="de").translate(text=event.raw_text)
-    formatted_text = translated_text + f"\n\n<a href='tg://privatepost?channel={str(event.chat_id)[4:]}&post={event.message.id}'>➡️ {config.CHANNELS[event.chat_id]}</a>"
-    print(translated_text)
+    text = translate(event)
 
     if type(event.original_update) is UpdateNewChannelMessage:
         print("send")
 
         if event.message.media is not None:
-
-            msg = await client.send_file(config.POST_CHANNEL, event.message.media, caption= formatted_text )
+            msg = await client.send_file(config.POST_CHANNEL, event.message.media, caption=text)
         else:
             msg = await client.send_message(config.POST_CHANNEL,
-                                            formatted_text)
+                                            text, link_preview=False)
 
         print(msg)
 
@@ -40,19 +72,18 @@ async def post_text(event: NewMessage.Event):
 
 @client.on(events.MessageEdited(chats=list(config.CHANNELS.keys()), incoming=True))
 @client.on(events.MessageEdited(chats=-1001391125365, outgoing=True))
-async def edit_text(event: NewMessage.Event):
+async def edit_text(event: MessageEdited.Event):
     print("edit--------", event.raw_text)
 
     print(event.stringify())
 
-    translated_text = GoogleTranslator(target="de").translate(text=event.raw_text)
-    print(translated_text)
+    text = translate(event)
 
     if type(event.original_update) is UpdateEditChannelMessage:
         print("update")
         post_id = get_post(event.chat_id, event.message.id)
         msg = await client.edit_message(config.POST_CHANNEL, post_id,
-                                        translated_text + f"\n\n<a href='tg://privatepost?channel={str(event.chat_id)[4:]}&post={event.message.id}'>➡️ {config.CHANNELS[event.chat_id]}</a>")
+                                        text)
         print(msg)
 
     # insert_post(Post(event.chat_id, event.message.id, msg.id))
