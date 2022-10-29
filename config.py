cd5e5f6e43c4caa3ef79e21ab3d1bc0944c1f989
nyx69 <loool1912@web.de>
2022-10-29 21:44:08 +0200

    more regex to filter out bloat

--- config.py
+++ config.py
@@ -1,79 +1,122 @@
 import dataclasses
 import os
 import re
+from typing import Optional
 
 from dotenv import load_dotenv
 
 load_dotenv()
-api_id = os.getenv("TG_ID")
-api_hash = os.getenv("TG_HASH")
+api_id = os.getenv('TG_ID')
+api_hash = os.getenv('TG_HASH')
 
-DATABASE_URL = "postgresql://postgres:area@localhost:5432/news"
+DATABASE_URL = 'postgresql://postgres:area@localhost:5432/news'
 POST_CHANNEL = -1001839268196
 
-FLAG_EMOJI = re.compile(r"🏴|🏳️|([🇦-🇿]{2})")
-PLACEHOLDER = "║"
+FLAG_EMOJI = re.compile(r'🏴|🏳️|([🇦-🇿]{2})')
+PLACEHOLDER = '║'
+
 
 @dataclasses.dataclass
 class Source:
     channel_name: str
-    footer: str = r"\s*"
-    bias: str = ""
+    bloat: Optional[list[str]] = None
+    bias: str = ''
 
 
 CHANNELS = {
-    # -1001839268196:Source("Test"),  # Test
-    # 1317941240:Source("MN-Bot",  # MN-bot
-    -1001391125365: Source("Test2"),  # Test2
-  #   -1001526741474:Source("MNChat"),  # MNChat
-    -1001486678205: Source("Militär-Memes", "🔰 Subscribe to @MilitaerMemes for more"),  # todo: also handle DE footer
-    -1001240262412: Source("Militär-News🔰", "🔰 Abonniere @MilitaerNews\n🔰 Diskutiere im @MNChat"),
+    # -1001839268196:Source('Test'),  # Test
+    # 1317941240:Source('MN-Bot',  # MN-bot
+    -1001391125365: Source('Test2',[
+        '<a href="https://t.me/+un4Mel3_naU1ZjZi">TEST-FOOTER</a>'
+                                    ]),  # Test2
+    #   -1001526741474:Source('MNChat'),  # MNChat
+    -1001486678205: Source('Militär-Memes', [
+        '🔰 Subscribe to @MilitaerMemes for more'
+    ]),  # todo: also handle DE footer
+    -1001240262412: Source('Militär-News🔰', [
+        '🔰 Abonniere @MilitaerNews\n🔰 Diskutiere im @MNChat'
+    ]),
 
     # Ukrainian
-    -1001452778151: Source("Krugozor", "<a href='https//t.me/krugozor_news'>Кругозор</a>", "🇺🇦"),
-    -1001619608359: Source("TiskSnip", bias="🇺🇦"),
-    -1001594428146: Source("WerochnaRada", bias="🇺🇦"),
-    -1001469021333: Source("DeepState", bias="🇺🇦"),
-    -1001759230769: Source("Bayraktar", "<a href='https://t.me/+WPl-WyPpW3QyMjgy'>Свежие крипто-новости тут</a>", "🇺🇦"),
-    -1001201459971: Source("Rysnya", bias="🇺🇦"),
-    -1001218743574: Source("Goncharenko", bias="🇺🇦"),
-    -1001413275904: Source("NEXTA", bias="🇺🇦"),
-    -1001011817559: Source("Militarnij", "@milinua", "🇺🇦"),
-    -1001414210176: Source("FirstDivision", bias="🇺🇦"),
+    -1001452778151: Source('Krugozor', [
+        '<a href="https://t.me/krugozor_news">Кругозор</a>'
+    ], '🇺🇦'),
+    -1001619608359: Source('TiskSnip', bias='🇺🇦'),
+    -1001594428146: Source('WerochnaRada', bias='🇺🇦'),
+    -1001469021333: Source('DeepState', bias='🇺🇦'),
+    -1001355310059: Source('EspresoTV', bias='🇺🇦'),
+    -1001759230769: Source('Bayraktar', [
+        '<a href="https://t.me/+WPl-WyPpW3QyMjgy">Свежие крипто-новости тут</a>'
+    ], '🇺🇦'),
+    -1001201459971: Source('Rysnya', bias='🇺🇦'),
+    # -1001218743574: Source('Goncharenko', bias='🇺🇦'),
+    -1001413275904: Source('NEXTA', [
+        'Прислать нам информацию: @nextamail_bot или info@nexta.tv\n\nПоддержать работу нашей редакции можно '
+        'через\nPayPal: https://paypal.me/nexta\nPatreon: https://patreon.com/nexta_tv\nИли другие варианты ('
+        'криптовалюта): https://nexta.tv/support\n\nНаш '
+        'YouTube\nhttps://www.youtube.com/nexta_tv\nhttps://www.youtube.com/nexta_live\n\nНаш Twitter (In '
+        'English)\nhttps://twitter.com/nexta_tv\n\nНаш TikTok\nwww.tiktok.com/@nexta_tv\n\nШойгу пошёл против Путина '
+        '/ Ну и новости!\nhttps://youtu.be/6T40_Ll1mMs\n\nФСБ готовит госпереворот / Ну и '
+        'новости!\nhttps://youtu.be/aIiUeuTxJJs '
+    ], bias='🇺🇦'),
+    -1001011817559: Source('Militarnij', [
+        '@milinua'
+    ], '🇺🇦'),
+    -1001414210176: Source('FirstDivision', bias='🇺🇦'),
 
     # Russian
-    -1001141171940: Source("Kadyrow", bias="🇷🇺"),
-    -1001076431027: Source("ItsDonezk",
-                           "<a href='https://t.me/joinchat/QCkIs9PWI4jWPgNy'>Подписаться</a>  |  <a href='https://t.me/EtoDonetsk_bot'>Предложить новость</a>",
-                           "🇷🇺"),
-    -1001260622817: Source("Readovka", bias="🇷🇺"),
-    -1001149896996: Source("Interfax", "@interfaxonline", "🇷🇺"),
-    -1001543896043: Source("RussiaMedwed",
-                           "Вся ЖЕСТЬ СВО у нас в резерве 👉 <a href='https://t.me/+bSe6VmuHMlthYWEy'>Смотреть</a>.",
-                           "🇷🇺"),
-    -1001135021433: Source("WarGonzo",
-                           "@wargonzo\n\n*наш проект существует на средства подписчиков, карта для помощи\n4279 3806 9842 9521",
-                           "🇷🇺"),
-    -1001708761316: Source("PrjamoyEfir", r"<a href='https://t.me/+jQvaOFH1zhAzMzIy'>Прямой эфир - подписаться</a>",
-                           "🇷🇺"),
-    -1001382288937: Source("BalkanSniper", bias="🇷🇺"),
-    -1001099737840: Source("Rusvesna",
-                           "<a href='<strong>t.me/rusvesnasu</strong>'><strong>t.me/rusvesnasu</strong></a>", "🇷🇺"),
-    -1001326223284: Source("Rybar", "@rybar\n\n<em>*Поддержать нас: </em><code>4377 7278 0407 7977</code>", "🇷🇺"),
-    -1001730870551: Source("RSOTM", bias="🇷🇺"),
+    -1001141171940: Source('Kadyrow', bias='🇷🇺'),
+    -1001076431027: Source('ItsDonezk', [
+        '<a href="https://t.me/joinchat/QCkIs9PWI4jWPgNy">Подписаться</a>  |  <a '
+        'href="https://t.me/EtoDonetsk_bot">Предложить новость</a> '
+    ], '🇷🇺'),
+    -1001260622817: Source('Readovka', bias='🇷🇺'),
+    -1001149896996: Source('Interfax', [
+        '@interfaxonline'
+    ], '🇷🇺'),
+    -1001543896043: Source('RussiaMedwed', [
+        'Вся ЖЕСТЬ СВО у нас в резерве 👉 <a href="https://t.me/+bSe6VmuHMlthYWEy">Смотреть</a>.'
+    ], '🇷🇺'),
+    -1001135021433: Source('WarGonzo', [
+        '@wargonzo\n\n*наш проект существует на средства подписчиков, карта для помощи\n4279 3806 9842 9521',
+        '@wargonzo\n\n<em>*наш проект существует на средства подписчиков, карта для помощи</em>\n4279 3806 9842 9521'
+    ], '🇷🇺'),
+    -1001708761316: Source('PrjamoyEfir',
+                           ['<a href="https://t.me/+jQvaOFH1zhAzMzIy">Прямой эфир - подписаться</a>'],
+                           '🇷🇺'),
+    -1001382288937: Source('BalkanSniper', [
+        '@balkanossiper — задаём мировые тренды с 28 июня 1914 года'
+    ], '🇷🇺'),
+    -1001099737840: Source('Rusvesna', [
+        '<a href="<strong>t.me/rusvesnasu</strong>"><strong>t.me/rusvesnasu</strong></a>'
+    ], '🇷🇺'),
+    -1001326223284: Source('Rybar', [
+        '@rybar\n\n<em>*Поддержать нас: </em><code>4377 7278 0407 7977</code>'
+    ], '🇷🇺'),
+    -1001730870551: Source('RSOTM', bias='🇷🇺'),
 
     # Polish
-    -1001577023152: Source("Syrenka", "@pl_syrenka — Польша не заграница", "🇵🇱"),
+    -1001577023152: Source('Syrenka', [
+
+        '@pl_syrenka — Польша не заграница'
+    ], '🇵🇱'),
 
     # Azeri
-    -1001261746950: Source("AAF",
-                           "<a href='http://t.me/military_az'><strong>AАF</strong></a><strong> | </strong><a href='https://t.me/joinchat/TCl5uh3G0Mhx42Kd'><strong>Чат24/7</strong></a><strong> |</strong><a href='https://t.me/AslanAslanoff'><strong>Сотрудничество</strong></a>",
-                           "🇦🇿"),
+    -1001261746950: Source('AAF', [
+        '<a href="http://t.me/military_az"><strong>AАF</strong></a><strong> | </strong><a '
+        'href="https://t.me/joinchat/TCl5uh3G0Mhx42Kd"><strong>Чат24/7</strong></a><strong> |</strong><a '
+        'href="https://t.me/AslanAslanoff"><strong>Сотрудничество</strong></a> '
+    ], '🇦🇿'),
 
     # Unclear
-    -1001658917464: Source("KhersonNonFake", "</strong><strong>@kherson_non_fake</strong>", "🇺🇦"),
-    -1001731636769: Source("Batalion «Monako»", "<a href='https://t.me/+un4Mel3_naU1ZjZi'>Батальон «Монако» 💎</a>"),
-    -1001602838096: Source("KronikaBpla", ),
-    -1001689244469: Source("UMF"),
+    -1001658917464: Source('KhersonNonFake', [
+        '</strong><strong>@kherson_non_fake</strong>',
+        'Точную инфу срочно сюда \n👉 </strong><strong>@non_fake_ks</strong>'
+    ], '🇺🇦'),
+    -1001731636769: Source('Batalion «Monako»', [
+        '<a href="https://t.me/+un4Mel3_naU1ZjZi">Батальон «Монако» 💎</a>'
+    ]),
+    -1001602838096: Source('KronikaBpla'),
+    -1001689244469: Source('UMF'),
 
 }
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 from deep_translator import GoogleTranslator
 from telethon import TelegramClient, events
 from telethon.events import NewMessage, MessageEdited
-from telethon.tl.types import UpdateNewChannelMessage, UpdateEditChannelMessage
+from telethon.tl.types import UpdateNewChannelMessage, UpdateEditChannelMessage, MessageMediaWebPage
 
 import config
 from db import insert_post, Post, get_post
@@ -12,23 +12,58 @@ client = TelegramClient("remove_inactive", config.api_id, config.api_hash)
 client.parse_mode = 'html'
 
 
+def debloat(event):
+    text = event.text
+
+    if config.CHANNELS[event.chat_id].bloat is not None:
+        bloats = [
+            e.replace("/", r"\/").replace(".", r"\.").replace("+", r"\+").replace("|", r"\|")
+            for e in config.CHANNELS[event.chat_id].bloat
+        ]  # .replace("<strong>","").replace("</strong>","")
+        print("bloats ::::", bloats, event.text)
+        if len(bloats) != 0:
+            for m in bloats:
+                text = re.sub(m, '', text)
+                print("replace::::::::::", text, "pattern ::::", m)
+
+    print("debloat ::::", text)
+    return text
+
+
+def sanitize(text: str):
+    hashtag = re.compile(r"\s*(\s*(#\w+))*\s*$")
+    empty_tag = re.compile(r"<(\w+)>\s*<\/\1>")
+    trailing_tag = re.compile(r"\s+<\/(\w+)>\s*$")
+
+    cleaned_empty =  re.sub(empty_tag, '',  text)
+
+    g = re.search(trailing_tag, cleaned_empty)
+    print("GROUP::::::::::::::::::::", cleaned_empty, g)
+
+    if g is not None:
+        for m in g.groups():
+            cleaned_empty = re.sub(trailing_tag, f"</{m}>", cleaned_empty, 1)
+            print("MATCH ::::::::::::::", cleaned_empty)
+
+    sub_text = re.sub(hashtag, '',cleaned_empty )
+    print("subbbbbbbb ", sub_text)
+
+    sub_text = sub_text.replace("<strong>", "<b>").replace("</strong>", "</b>").replace("<em>", "<i>").replace("</em>", "</i>")
+
+    print("sanitized ::::", sub_text)
+    return sub_text
+
+
 def translate(event):
     if event.message.fwd_from is not None and event.message.fwd_from.from_id in list(config.CHANNELS.keys()):
         return
 
-    footer = config.CHANNELS[event.chat_id].footer.replace("/", r"\/").replace(".", r"\.").replace("+", r"\+").replace(
-        "'", "\"").replace("|", r"\|")
-    print(footer)
-    if footer != "":
-        matches = re.findall(footer, event.text)
-        print(matches, event.text)
-        if len(matches) == 0:
-            return  # because some have their ads without footer
+    sub_text = sanitize( debloat(event))
 
-    sub_text = re.sub(r"\s*((#\w+)*\s*)*\s*" + footer + r"\s*$", '', event.text)
     emojis = re.findall(config.FLAG_EMOJI, sub_text)
     text_to_translate = re.sub(config.FLAG_EMOJI, config.PLACEHOLDER, sub_text)
     translated_text = GoogleTranslator(target="de").translate(text=text_to_translate)
+
     for emoji in emojis:
         translated_text = re.sub(config.PLACEHOLDER, emoji, translated_text, 1)
 
@@ -49,7 +84,6 @@ async def handle_album(event):  # craft a new message and send
 @client.on(events.NewMessage(chats=-1001391125365, outgoing=True))
 async def post_text(event: NewMessage.Event):
     print(event.raw_text)
-
     print(event.stringify())
 
     text = translate(event)
@@ -57,11 +91,11 @@ async def post_text(event: NewMessage.Event):
     if type(event.original_update) is UpdateNewChannelMessage:
         print("send")
 
-        if event.message.media is not None:
+        if (
+                event.photo is not None or event.video is not None) and type(event.media) is not MessageMediaWebPage:  # filter for media type???
             msg = await client.send_file(config.POST_CHANNEL, event.message.media, caption=text)
         else:
-            msg = await client.send_message(config.POST_CHANNEL,
-                                            text, link_preview=False)
+            msg = await client.send_message(config.POST_CHANNEL, text, link_preview=False)
 
         print(msg)
 
@@ -82,8 +116,12 @@ async def edit_text(event: MessageEdited.Event):
     if type(event.original_update) is UpdateEditChannelMessage:
         print("update")
         post_id = get_post(event.chat_id, event.message.id)
-        msg = await client.edit_message(config.POST_CHANNEL, post_id,
-                                        text)
+        print("post to update :::::::::::", post_id, type(post_id))
+
+        if post_id is None:
+            return await post_text(event)
+
+        msg = await client.edit_message(config.POST_CHANNEL, post_id, text, link_preview=False)
         print(msg)
 
     # insert_post(Post(event.chat_id, event.message.id, msg.id))
